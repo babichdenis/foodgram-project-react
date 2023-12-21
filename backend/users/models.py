e5ec0b95f8259beedf9d8bfbb1c0abdf387e4c07
babichdenis <babichdenis@me.com>
2023-12-21 14:59:26 +0300

    9

--- backend/users/models.py
+++ backend/users/models.py
@@ -11,7 +11,7 @@ class CustomUser(AbstractUser):
     """
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']    
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     username = models.CharField(
         "Логин",
