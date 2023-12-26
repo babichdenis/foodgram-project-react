23fa51ac2ea08ee497f443b3f96f17e4818ebd60
babichdenis <babichdenis@me.com>
2023-12-26 08:36:54 +0300

    81

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,5 +16,5 @@ router.register('users', CustomUserViewSet, basename='users')
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
+#    path('', include('djoser.urls')),
 ]
