eb3d020537fabb354263bb3b80e087b20c524850
babichdenis <babichdenis@me.com>
2023-12-29 11:20:23 +0300

    85

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,8 @@
-from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
+
 from recipes.models import Ingredient, Recipe, Tag
+
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,15 @@
-from api.pagination import CustomPagination
-from api.serializers import CustomUserSerializer, SubscribeSerializer
-from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
 from .models import Subscribe
+from api.pagination import CustomPagination
+from api.serializers import CustomUserSerializer, SubscribeSerializer
 
 User = get_user_model()
 
