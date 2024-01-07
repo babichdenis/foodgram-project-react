cca1de1d8d7bd5ef345cae047d2d7d1dbd167b7e
babichdenis <babichdenis@me.com>
2024-01-08 01:42:20 +0300

    122

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -34,16 +34,11 @@ class UserAdmin(admin.ModelAdmin):
         return obj.recipes.count()
 
 
-'''
 @admin.register(Subscription)
 class Subscription(admin.ModelAdmin):
-    list_display = ('user', 'author')
-    search_fields = (
-        'author__username',
-        'author__email',
-        'user__username',
-        'user__email'
-    )
-    list_filter = ('author__username', 'user__username')
+    """Административная панель для управления подписками."""
+
+    list_display = ("user", "author")
+    list_filter = ("user", "author")
+    search_fields = ("user", "author")
     empty_value_display = EMPTY_MESSAGE
- '''
