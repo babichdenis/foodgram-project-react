2101a3fffbbdeda1b42889774760c217961b8c3a
babichdenis <babichdenis@me.com>
2024-01-18 02:45:56 +0300

    Final_10.1

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Subscription
 
 admin.site.empty_value_display = "Не задано"
 
@@ -16,6 +16,8 @@ class UserAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
         'date_joined',
+        'count_subscribers',
+        'count_recipes'
     )
     search_fields = (
         'email',
@@ -30,7 +32,19 @@ class UserAdmin(admin.ModelAdmin):
     ordering = ('username', )
 
     def count_subscribers(self, obj):
-        return obj.subscriber.count() + 1000
+        return obj.subscriber.count()
 
     def count_recipes(self, obj):
-        return obj.recipes.count() + 1000
+        return obj.recipes.count()
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = (
+        'author__username',
+        'author__email',
+        'user__username',
+        'user__email'
+    )
+    list_filter = ('author__username', 'user__username')
