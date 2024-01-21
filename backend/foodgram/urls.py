c1a61c1e2d78543528d0b5cf5cb32b235eb5ed85
babichdenis <babichdenis@me.com>
2024-01-21 18:48:28 +0300

    Final_13.1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,22 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-# from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
     urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-'''     path(
-        "redoc/",
-        TemplateView.as_view(template_name="redoc.html"),
-        name="redoc"
-    ), '''
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
