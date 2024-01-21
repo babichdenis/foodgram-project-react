680442a3e3ccc0644e830b38853fa5a055fdf282
babichdenis <babichdenis@me.com>
2024-01-21 14:26:58 +0300

    Final_12.6

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,8 +12,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
