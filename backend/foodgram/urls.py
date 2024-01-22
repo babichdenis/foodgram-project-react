b5559ec03387ea158c4809942e4580f0d28168d1
babichdenis <babichdenis@me.com>
2024-01-22 17:10:15 +0300

    Final_1.7

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(
+        "redoc/",
+        TemplateView.as_view(template_name="redoc.html"),
+        name="redoc"
+    ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
