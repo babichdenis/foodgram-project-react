341b7158c51a3b3b0a87306c92a430abbfd8d7d2
babichdenis <babichdenis@me.com>
2024-02-01 16:05:37 +0300

    1.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.paginations.Pagination'
+    'DEFAULT_PAGINATION_CLASS': 'api.paginations.Pagination',
     #    "DEFAULT_PAGINATION_CLASS": (
     #        "rest_framework.pagination.PageNumberPagination"),
     "PAGE_SIZE": os.getenv("PAGE_SIZE", "6"),
