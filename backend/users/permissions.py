2a2c4b6a84fbc39ddf0b8173577c2226fe95f2d1
babichdenis <babichdenis@me.com>
2024-02-03 02:47:48 +0300

    Update permissions.py

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -1,26 +1,21 @@
 from rest_framework import permissions
-from rest_framework.permissions import SAFE_METHODS
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    """Админу: полный доступ, остальным: только чтение."""
-
     def has_permission(self, request, view):
-
-        return request.method in SAFE_METHODS or request.user.is_staff
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_staff
+        )
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Автору объекта: полный доступ,
-    анониму: безопасные методы,
-    аутентифицированному: доступ, но без доступа к объекту.
-    """
-
     def has_permission(self, request, view):
-
-        return request.method in SAFE_METHODS or request.user.is_authenticated
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user == obj.author
+        )
